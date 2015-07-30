69496f086cf33e4f02dd918557d04b209e632340
Russell Nield <russell@ft-wd.com>
2015-07-30 13:10:08 -0700

    fixing html output in question detail

--- app/app.js
+++ app/app.js
@@ -4,9 +4,3 @@ angular.module('kbase', ['ngRoute','ui.bootstrap','ngAnimate','ngSanitize']);
 angular.module('kbase').controller('CollapseCtrl', function ($scope) {
 	$scope.isCollapsed = true;
 });
-
-angular.module('app').filter('to_trusted', ['$sce', function($sce){
-	return function(text) {
-		return $sce.trustAsHtml(text); 
-	};	
-}]);
