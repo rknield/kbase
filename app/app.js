416f802790b4139b3279916d0bdd3bf141af9e20
Russell Nield <russell@ft-wd.com>
2015-07-30 13:43:00 -0700

    fixing html output in question detail

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 "use strict";
-angular.module('kbase', ['ngRoute','ui.bootstrap','ngAnimate','ngSanitize']);
+angular.module('kbase', ['ngRoute');
 
 angular.module('kbase').controller('CollapseCtrl', function ($scope) {
 	$scope.isCollapsed = true;
