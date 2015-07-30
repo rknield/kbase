0aa604fdaac82fd8877cb9bf618e5833e3252b61
Russell Nield <russell@ft-wd.com>
2015-07-30 13:03:24 -0700

    fixing html output in question detail

--- app/app.js
+++ app/app.js
@@ -7,6 +7,6 @@ angular.module('kbase').controller('CollapseCtrl', function ($scope) {
 
 angular.module('app').filter('to_trusted', ['$sce', function($sce){
 	return function(text) {
-		return $sce.trustAsHtml(text);
-	};
+		return $sce.trustAsHtml(text); 
+	};	
 }]);
