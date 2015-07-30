ee0622a0d52176620a55f0164db2bb2f0058c9bf
Russell Nield <russell@ft-wd.com>
2015-07-30 06:17:53 -0700

    Updating file paths for templates

--- app/app.routes.js
+++ app/app.routes.js
@@ -2,11 +2,11 @@ angular.module('kbase').config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider
 		.when('/', {
-			templateUrl: '/app/components/home/questions-list.html',
+			templateUrl: 'app/components/home/questions-list.html',
 			controller: 'MainCtrl'
 		})
 		.when('/question/:questionID', {
-			templateUrl: '/app/components/detail/question.html',
+			templateUrl: 'app/components/detail/question.html',
 			controller: 'AnswerCtrl'
 		})
 		.otherwise({
