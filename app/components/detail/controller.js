890bccb4f6c97359153ed0daaf6993b55de59a51
Russell Nield <russell@ft-wd.com>
2015-07-30 17:30:25 -0700

    fixing questions and answers

--- app/components/detail/controller.js
+++ app/components/detail/controller.js
@@ -1,30 +1,29 @@
 angular.module('kbase')
-	/*.directive('onLastRepeat', function() {
+	.directive('onLastRepeat', function() {
         return function(scope, element, attrs) {
             if (scope.$last) setTimeout(function(){
                 scope.$emit('onRepeatLast', element, attrs);
             }, 1);
         };
-    })*/
+    })
 	.controller('AnswerCtrl', ['$scope','$routeParams','$http','$sce', function ($scope, $routeParams, $http, $sce) {
 		var questionid = $routeParams.questionID;
 	
 		$http.get('http://www.ft-wd.com/kbaseConn/kbase.php?r=qa&qid=' + questionid)
-			.success(function(response){
-				$scope.questions = response.questions;
-				$scope.answers = response.answers;
-				$scope.count = $scope.answers.length;
+		.success(function(response){
+			$scope.questions = response.questions;
+			$scope.answers = response.answers;
+			$scope.count = $scope.answers.length;
 
-				console.log(response.questions);
-				console.log(response.answers);
-			})
-			.error(function(data,status,headers,config){
-				console.log("there was an error"); 
-			});
+			console.log(response.questions);
+			console.log(response.answers);
+		})
+		.error(function(data,status,headers,config){
+			console.log("there was an error"); 
+		});
 
-			/*$scope.$on('onLastRepeat', function(scope, element, attrs){
-				console.log("im done");
-			});*/
-		
+		$scope.$on('onLastRepeat', function(scope, element, attrs){
+			console.log("im done");
+		});
 	}
 ]);
