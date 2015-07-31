11638546ebbcbefa800c4b991b4f767b0b2eb78c
Russell Nield <russell@ft-wd.com>
2015-07-30 17:01:43 -0700

    fixing questions and answers

--- app/components/detail/controller.js
+++ app/components/detail/controller.js
@@ -1,23 +1,30 @@
-angular.module('kbase').controller('AnswerCtrl', ['$scope','$routeParams','$http','$sce', function ($scope, $routeParams, $http, $sce) {
+angular.module('kbase')
+	.directive('onLastRepeat', function() {
+        return function(scope, element, attrs) {
+            if (scope.$last) setTimeout(function(){
+                scope.$emit('onRepeatLast', element, attrs);
+            }, 1);
+        };
+    })
+    .controller('AnswerCtrl', ['$scope','$routeParams','$http','$sce', function ($scope, $routeParams, $http, $sce) {
 		var questionid = $routeParams.questionID;
 	
 		$http.get('http://www.ft-wd.com/kbaseConn/kbase.php?r=qa&qid=' + questionid)
-			 .success(function(response){
+			.success(function(response){
 				$scope.questions = response.questions;
 				$scope.answers = response.answers;
-				
 				$scope.count = $scope.answers.length;
 
 				console.log(response.questions);
 				console.log(response.answers);
-			 })
-			 .error(function(data,status,headers,config){
+			})
+			.error(function(data,status,headers,config){
 				console.log("there was an error"); 
-			 });
+			});
 
-		//$scope.questions[0].body = $sce.trustAsHtml($scope.questions[0].body);
-		//s$scope.answers.body = $sce.trustAsHtml($scope.answers.body);
-
-		//console.log($scope.questions); 
+			$scope.$on('onRepeatLast', function(scope, element, attrs){
+				console.log("im done");
+			});
+		
 	}
 ]);
